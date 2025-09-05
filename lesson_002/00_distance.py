abe54dd67c7aad2fe15e1c98034fe299950bb6a8
albug020 <albugrezov@gmail.com>
2025-09-05 06:01:29 +0400

    Выполнил задание distance и удалил все TODO

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pprint import pprint
+
 # Есть словарь координат городов
 
 sites = {
@@ -12,11 +14,32 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+# distances = {}
+distances = dict()
 
-# TODO здесь заполнение словаря
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
 
-print(distances)
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** .5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - london[1]) ** 2)  ** .5
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] -paris[1]) ** 2) ** .5
+
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+
+distances['London'] = {}
+distances['London']['Moscow'] = moscow_london
+distances['London']['Paris'] = london_paris
+
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = moscow_paris
+distances['Paris']['London'] = london_paris
+
+pprint(distances)
+
+print(distances['Moscow']['Paris'])
 
 
 
